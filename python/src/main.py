bdba1befce1b7fe53b38ea62c695c754d563c4bf
koba-masa <kobayashi_mshr@koba-masa.com>
2022-06-14 22:40:35 +0900

    tmp

--- python/src/main.py
+++ python/src/main.py
@@ -4,27 +4,26 @@ from gpio import GPIO
 import time
 
 class Koyanagi:
-    def __init__(self):
-        # 測定中のランプ
-        self.processed_lamp = GPIO.GPIO_26
-        # 開始ボタン
-        self.start_btn = GPIO.GPIO_19
+  def __init__(self):
+    # 測定中のランプ
+    self.processed_lamp = GPIO.GPIO_26
+    # 開始ボタン
+    self.start_btn = GPIO.GPIO_19
 
-        RPi.GPIO.setmode(RPi.GPIO.BCM)
-        RPi.GPIO.setup(self.processed_lamp, RPi.GPIO.OUT)
-        RPi.GPIO.setup(self.start_btn, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
+    RPi.GPIO.setmode(RPi.GPIO.BCM)
+    RPi.GPIO.setup(self.processed_lamp, RPi.GPIO.OUT)
+    RPi.GPIO.setup(self.start_btn, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
 
-    def execute(self):
-        RPi.GPIO.output(self.processed_lamp, 0)
-        #while true:
-        #    RPi.GPIO.output(self.processed_lamp, 1)
-        #    time.sleep(1)
-        #    RPi.GPIO.output(self.processed_lamp, 0)
-        #    time.sleep(1)
+  def execute(self):
+    RPi.GPIO.output(self.processed_lamp, 0)
+    #while true:
+    #    RPi.GPIO.output(self.processed_lamp, 1)
+    #    time.sleep(1)
+    #    RPi.GPIO.output(self.processed_lamp, 0)
+    #    time.sleep(1)
 
-    def __del__(self):
-        RPi.GPIO.cleanup(self.processed_lamp)
-        RPi.GPIO.cleanup(self.start_btn)
-
+  def __del__(self):
+    RPi.GPIO.cleanup(self.processed_lamp)
+    RPi.GPIO.cleanup(self.start_btn)
 
 Koyanagi().execute()
