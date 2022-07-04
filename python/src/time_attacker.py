452e914fca061f3f8354d59a6e725bf443b61cf2
koba-masa <kobayashi_mshr@koba-masa.com>
2022-07-04 12:53:22 +0900

    refactor: メソッド分割

--- python/src/time_attacker.py
+++ python/src/time_attacker.py
@@ -8,9 +8,9 @@ from device.aqm0802a import AQM0802A
 class TimeAttacker:
   def __init__(self):
     # 測定中のランプ
-    self.processed_lamp = 26
+    self.processed_lamp = 19
     # 開始ボタン
-    self.start_btn = 19
+    self.start_btn = 26
     # 終了ボタン
     self.finish_btn = 21
 
@@ -25,6 +25,7 @@ class TimeAttacker:
     GPIO.output(self.processed_lamp, GPIO.LOW)
 
     self.lcd = AQM0802A()
+    self.lcd.light_on()
 
     try:
       while True:
@@ -32,6 +33,7 @@ class TimeAttacker:
     except Exception as e:
       print(e)
     finally:
+      self.lcd.light_off()
       GPIO.cleanup(self.processed_lamp)
       GPIO.remove_event_detect(self.start_btn)
       GPIO.cleanup(self.start_btn)
@@ -43,19 +45,34 @@ class TimeAttacker:
   def start(self, gpio):
     if GPIO.input(self.processed_lamp) == 0:
       print("start")
+      self.lcd.reset()
       self.lcd.display_upper('start')
       self.start = time.perf_counter()
-      GPIO.output(self.processed_lamp, GPIO.HIGH)
+      self.turn_on_off_led(GPIO.HIGH)
 
   def finish(self, gpio):
     if GPIO.input(self.processed_lamp) == 1:
       print("finish")
       self.lcd.reset()
       self.end = time.perf_counter()
-      self.attack_time = math.ceil(self.end - self.start)
-      self.lcd.display_upper(str(self.attack_time))
-      GPIO.output(self.processed_lamp, GPIO.LOW)
-      self.start = 0
-      self.end = 0
+      self.attack_time = self.calculate(self.start, self.end)
+      self.display(self.attack_time)
+      self.turn_on_off_led(GPIO.LOW)
+      self.reset()
+
+  def reset(self):
+    self.start = 0
+    self.end = 0
+
+  def turn_on_off_led(self, up_down):
+    GPIO.output(self.processed_lamp, up_down)
+
+  def calculate(self, start_time, end_time):
+    # TODO 1/10秒まで計測する
+    return math.ceil(end_time - start_time)
+
+  def display(self, contents):
+    self.lcd.display_upper(str(contents))
+    self.lcd.display_lower('     sec')
 
 TimeAttacker().execute()
