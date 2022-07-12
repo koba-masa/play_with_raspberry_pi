c3ebdba8d0502c00ebe40cf1594ec264898e7633
koba-masa <kobayashi_mshr@koba-masa.com>
2022-07-12 21:07:40 +0900

    refactor: Imageオブジェクトの生成をSSD1306から分離

--- python/src/device/ssd1306.py
+++ python/src/device/ssd1306.py
@@ -3,7 +3,6 @@
 import board
 import busio
 import adafruit_ssd1306
-from PIL import Image, ImageDraw, ImageFont
 
 import time
 
@@ -11,34 +10,23 @@ import time
 # https://micropython-docs-ja.readthedocs.io/ja/latest/esp8266/tutorial/ssd1306.html
 # https://learn.adafruit.com/monochrome-oled-breakouts/python-setup
 class SSD1306:
-  def __init__(self, slave_address, vertical_pixcel, horizontal_pixcel):
+  def __init__(self, slave_address, width, hight):
     self.slave_address = slave_address
-    self.vertical_pixcel = vertical_pixcel
-    self.horizontal_pixcel = horizontal_pixcel
+    self.width = width
+    self.hight = hight
     self.setup()
 
   def setup(self):
     self.i2c = busio.I2C(board.SCL, board.SDA)
-    self.oled = adafruit_ssd1306.SSD1306_I2C(self.vertical_pixcel, self.horizontal_pixcel, self.i2c, addr=self.slave_address)
+    self.oled = adafruit_ssd1306.SSD1306_I2C(self.width, self.hight, self.i2c, addr=self.slave_address)
     self.clear()
-    self.font = ImageFont.load_default()
 
   def clear(self):
     self.oled.fill(0)
     self.oled.show()
 
-  def write(self, text):
-    # https://pillow.readthedocs.io/en/stable/reference/Image.html#constructing-images
-    self.image = Image.new('1', (self.oled.width, self.oled.height), 0)
-    self.draw = ImageDraw.Draw(self.image)
-    font_width, font_height = self.font.getsize(text)
-    self.draw.text(
-        (self.oled.width // 2 - font_width // 2, self.oled.height // 2 - font_height // 2),
-        text,
-        font=self.font,
-        fill=255,
-    )
-    self.oled.image(self.image)
+  def write(self, image):
+    self.oled.image(image)
     self.oled.show()
 
   def reset(self):
